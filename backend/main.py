ae205d5e018a3465c6d66a9b5eb67deaf40a6201
Francisco Ortega <ortega.fo53@gmail.com>
2025-05-26 08:32:50 -0700

    setup for tailwind & simple fetch request

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
-
+from flask_cors import CORS
 from flask import Flask, request, jsonify
 from routes import routes
 
 app = Flask(__name__)
 app.register_blueprint(routes)
+CORS(app,origins=["http://localhost:5050"])
 
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to </h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+import { onMount } from "svelte";
+const localhost= "http://localhost:8000"
+const request_path =localhost+"/fliers"
+
+let fliers =$state([]);
+onMount(async function () {
+let rquestFlier= await fetch(request_path).then(data=> data.json())
+  .then(data=>{
+    fliers=data
+})
+})
+</script>
+
+
+
+
+
+
+
+
+
+<div class="h-screen w-full  flex flex-col  items-center justify-around gap-30">
+<h1 class="font-bold 
+text-5xl
+">OpenShelf</h1>
+
+<div
+class="w-9/10 grid grid-flow-row grid-cols-4 gap-4 "
+>
+{#each fliers as flier}
+
+ <div class="max-w-sm bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300 border-solid border-1">
+    <img
+ src={flier.image_url}
+      class="w-full  h-120 object-contain "
+    />
+    <div class="p-4">
+      <p class="text-lg font-semibold text-gray-800">
+	{flier.title}
+	          id: {flier.id}
+      </p>
+    </div>
+  </div>
+{:else}
+	<p>loading..</p>
+{/each}
+</div>
+</div>
