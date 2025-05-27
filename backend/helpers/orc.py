5c55ef71a256af1e440a1c6d12ccd8adabf72e27
Francisco Ortega <ortega.fo53@gmail.com>
2025-05-27 10:21:00 -0700

    orc setup

--- backend/helpers/orc.py
+++ backend/helpers/orc.py
@@ -1,5 +1,15 @@
 import datetime
+from pathlib import Path
+import json, sys
+import easyocr
+import os
 
+def imageToJson(path, lang="en"):
+    reader = easyocr.Reader([lang])
+    lines  = reader.readtext(path, detail=0, paragraph=True)
+    result =  " ".join(lines)
+
+    return result
 
 def extractTextFromImage(request):
 
@@ -11,7 +21,11 @@ def extractTextFromImage(request):
     user_id =data['user_id']
 
     title =f'{uploaded_file.filename}-{now}' 
+    file_path=f"uploads/{uploaded_file.filename}"
+    uploaded_file.save(file_path)
+    result= imageToJson(file_path)
+    os.remove(file_path)
     temp_url="https://rccd.edu/news/publishing-images/2022_2023/STEM_NASA_FLYER.png"
-    return {"title":title,"user_id":user_id,"description": "placeholder description","image_url": temp_url}
+    return {"title":title,"user_id":user_id,"description": result,"image_url": temp_url}
 
 
--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,11 @@ print(os.getenv("FLASK_ENV"))
 app = Flask(__name__)
 app.register_blueprint(routes)
 CORS(app,origins=["*"])
-
+# Configure the upload folder
+UPLOAD_FOLDER = 'uploads'  # Directory to save uploaded files
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# Ensure the upload folder exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 if __name__ == '__main__':
     app.run()
 
